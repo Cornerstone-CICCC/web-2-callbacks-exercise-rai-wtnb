d2e42840d23aa08288cdd365156fe29ee6b0e4b1
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-11-18 20:11:11 -0800

    chore: finish

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+const charset = "utf-8";
+
+fs.readFile("./firstname.txt", charset, (err, firstname) => {
+  if (err) throw err;
+  fs.readFile("./lastname.txt", charset, (err, lastname) => {
+    if (err) throw err;
+    fs.readFile("./age.txt", charset, (err, age) => {
+      if (err) throw err;
+      fs.readFile("./hobbies.txt", charset, (err, hobbiesData) => {
+        if (err) throw err;
+        const hobbies = JSON.parse(hobbiesData);
+        console.log(
+          `${firstname} ${lastname} is ${age} years old and his hoobies are ${hobbies[0]} and ${hobbies[1]}`
+        );
+      });
+    });
+  });
+});
